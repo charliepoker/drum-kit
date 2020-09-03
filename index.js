1159025805af4cd590e32eb89378e1779cf07a11
Iheanacho charles <iheanachocharlie@gmail.com>
2020-09-03 21:10:39 +0100

    Add all files

---
+++ index.js
@@ -0,0 +1,10 @@
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numOfDrumButtons; i++) {
+  document
+    .querySelectorAll(".drum")
+    [i].addEventListener("click", function () {});
+}
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
